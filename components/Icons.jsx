c27b5b5a9bf8d87452188f29b340b5a1269d09e3
Hector Sanchez <hola@hectorsanchez.mx>
2022-08-29 19:32:57 -0500

    Adds icons and Ubuntu font

--- components/Icons.jsx
+++ components/Icons.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 
 const Icons = () => {
   return (
-    <section className='icons-wrapper max-w-4xl my-0 mx-auto'>
+    <section className='icons-wrapper max-w-5xl my-0 mx-auto'>
         <div className='iconos mt-14 flex justify-between'>
             <ul className='card w-56'>
-                <li className='icon-card w-16 h-16 rounded-3xl grid place-items-center mb-4 text-2xl'></li>
+                <li className='icon-card w-16 h-16 rounded-3xl grid place-items-center mb-4 text-2xl'><span className='icon-diamond'/></li>
                 <li className='icon-header no-underline border-none'>
                     <h3 className='text-xl font-bold'>
                         Aumenta
@@ -20,7 +20,7 @@ const Icons = () => {
             </ul>
 
             <ul className='card w-56'>
-                <li className='icon-card w-16 h-16 rounded-3xl grid place-items-center mb-4 text-2xl'></li>
+                <li className='icon-card w-16 h-16 rounded-3xl grid place-items-center mb-4 text-2xl'><span className='icon-rocket'/></li>
                 <li className='icon-header no-underline border-none'>
                     <h3 className='text-xl font-bold'>
                         Identifica tus áreas
@@ -35,7 +35,7 @@ const Icons = () => {
             </ul>
 
             <ul className='card w-56'>
-                <li className='icon-card w-16 h-16 rounded-3xl grid place-items-center mb-4 text-2xl'></li>
+                <li className='icon-card w-16 h-16 rounded-3xl grid place-items-center mb-4 text-2xl'><span className='icon-award'/></li>
                 <li className='icon-header no-underline border-none'>
                     <h3 className='text-xl font-bold'>
                         Certifica
@@ -43,7 +43,7 @@ const Icons = () => {
                         tus conocimientos
                     </h3>
                 </li>
-                <li className='divider w-20 h-1 bg-violet-600 rounded m-3 mx-0'></li>
+                <li className='divider w-20 h-1 bg-violet-600 rounded m-3 mx-0'><span className='icon-lightning'/></li>
                 <li className='card-desk no-underline border-none text-sm block '>
                     <p>Contamos con programas avalados por instituciones nacionales e internacionales.</p>
                 </li>
@@ -66,8 +66,8 @@ const Icons = () => {
 
         </div>
     </section>
-    
-    
+
+
   )
 }
 
--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,27 @@
 import Container from "../components/Container"
 import Icons from "../components/Icons"
-import Aliados from "../components/aliados"
+import Aliados from "../components/Aliados"
+import Hero from "../components/Hero"
+import Cursos from "../components/Cursos"
 
-
-
-
+import Head from 'next/head'
 
 export default function Home() {
   return (
-    <><Container /><Icons /><Aliados /></>
-
-    
+      <>
+          <Head>
+              <link rel="preconnect" href="https://fonts.googleapis.com"/>
+              <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin/>
+              <link href="https://fonts.googleapis.com/css2?family=Ubuntu:wght@300;400;500;700&display=swap"
+                    rel="stylesheet"/>
+              <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/simple-line-icons/2.5.5/css/simple-line-icons.min.css"/>
+          </Head>
+          <Hero/>
+          {/*<Container />*/}
+          <Icons />
+          <Aliados />
+          <Cursos />
+      </>
   )
-  
+
 }
